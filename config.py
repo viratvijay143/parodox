6d0e8dde855e43f6512bdf226bb7b8f2139c3a7a
SAHU <161798305+viratvijay143@users.noreply.github.com>
2024-09-26 12:11:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,9 +2,9 @@ import os
 from os import getenv
 
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7261086621:AAFTjL9QjILoXUxHvhyrIeHJmdDUPP1boqw")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7417717621:AAFibMWs3uejBzUPJrljDww0zPq7NyQQo1M")
 API_ID = getenv("API_ID", "23108855")
 API_HASH = getenv("API_HASH", "ee97c1084e29530c019b13c5778ab2db")
 sudo_group = -1002116155974
 sudo_user = 5498931783
-log_channel = -1002248917171
+log_channel = -1002116155974
